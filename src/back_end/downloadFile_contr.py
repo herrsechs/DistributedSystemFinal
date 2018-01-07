30eaa4fdee7bea2dfe791a3d92d6350394c780be
Liu Yajun <herrsechs@foxmail.co>
2018-01-08 00:17:43 +0800

    download file from slaves

--- src/back_end/downloadFile_contr.py
+++ src/back_end/downloadFile_contr.py
@@ -1,6 +1,6 @@
 from django.http import StreamingHttpResponse
 from file_trans.models import DataFile
-from ip_mapping import get_chunk_from_slave
+from ip_mapping import get_chunk_from_slave, slave_ids, remote_dirs, remote_usrs, get_ip
 import os
 
 
@@ -9,40 +9,31 @@ def read_file(filename):
     chunk_num = df.chunk_size  # Number of chunks
     slice_num = chunk_num / 5
 
-    slave_ids = ['10.60.45.10', '10.60.45.19', '10.60.45.22']
-    remote_usrs = ['liuyajun', 'lyj', 'jishi511']
-    remote_dirs = ['/home/liuyajun/data_files/',
-                   '/home/lyj/data_files/',
-                   '/large_disk/lyj/data_files/']
-
+    local_ip = get_ip()
     for i in range(chunk_num):
         local_path = os.path.join('data_files', filename)
 
-        if i < slice_num * 3:
-            filepath = 'data_files/' + filename + str(i)
-            with open(filepath, "rb") as fr:
+        # Mapping server ip to chunk index
+        server_list = []
+        if i < slice_num:
+            server_list.extend([0, 3])
+        elif slice_num <= i < slice_num * 2:
+            server_list.extend([0, 1])
+        elif slice_num * 2 <= i < slice_num * 3:
+            server_list.extend([0, 2])
+        elif slice_num * 3 <= i < slice_num * 4:
+            server_list.extend([1, 3])
+        else:
+            server_list.extend([2, 3])
+
+        if local_ip == slave_ids[server_list[0]] or local_ip == slave_ids[server_list[1]]:
+            filepath = os.path.join(remote_dirs[slave_ids.index(local_ip)], filename + str(i))
+            with open(filepath, 'rb') as fr:
                 c = fr.read()
                 yield c
-        elif slice_num * 3 <= i < slice_num * 4:
+        else:
             flag = False
-            for j in [0,2]:
-                flag = get_chunk_from_slave(slave_ids[j],
-                                            local_path+str(i),
-                                            os.path.join(remote_dirs[j], filename + str(i)),
-                                            remote_usrs[j])
-                if flag:
-                    break
-                else:
-                    print('Cannot connect to server %s when requiring chunk %i' % (slave_ids[i], i))
-            if flag:
-                filepath = 'data_files/' + filename + str(i)
-                with open(filepath, "rb") as fr:
-                    c = fr.read()
-                    yield c
-                os.remove(filepath)
-        elif slice_num * 4 <= i < slice_num * 5:
-            flag = False
-            for j in [1, 2]:
+            for j in server_list:
                 flag = get_chunk_from_slave(slave_ids[j],
                                             local_path + str(i),
                                             os.path.join(remote_dirs[j], filename + str(i)),
@@ -52,7 +43,7 @@ def read_file(filename):
                 else:
                     print('Cannot connect to server %s when requiring chunk %i' % (slave_ids[i], i))
             if flag:
-                filepath = 'data_files/' + filename + str(i)
+                filepath = os.path.join(remote_dirs[slave_ids.index(local_ip)], filename + str(i))
                 with open(filepath, "rb") as fr:
                     c = fr.read()
                     yield c
