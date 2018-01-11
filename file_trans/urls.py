1566688819621c8f5b0e1b5b6e5cf98cfe5374b7
Liu Yajun <herrsechs@foxmail.co>
2018-01-11 20:38:13 +0800

    Correct url patterns

--- file_trans/urls.py
+++ file_trans/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     url(r'^download/$',views.download_file,name='download'),
     url(r'^update/$', views.update_file, name='update'),
     url(r'^delete/$', views.delete_file, name='delete'),
-    url(r'^charts(?P<index>[0-9]+)/$', views)
+    url(r'^charts(?P<index>[0-9]+)/$', views.show_charts, name='show_charts')
 ]
